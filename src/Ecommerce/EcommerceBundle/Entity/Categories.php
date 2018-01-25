dbd0b4c605d403db295b805cdc3706db01ee6ff8
imadealmorabit <imadealmorait@gmail.com>
2018-01-25 22:45:26 +0000

    creation de la relation entre Categories et Media

--- src/Ecommerce/EcommerceBundle/Entity/Categories.php
+++ src/Ecommerce/EcommerceBundle/Entity/Categories.php
@@ -22,6 +22,12 @@ class Categories
     private $id;
 
     /**
+     * @ORM\OneToOne(targetEntity="Ecommerce\EcommerceBundle\Entity\Media", cascade={"persist","remove"})
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $image;
+
+    /**
      * @var string
      *
      * @ORM\Column(name="nom", type="string", length=255)
@@ -63,4 +69,3 @@ class Categories
         return $this->nom;
     }
 }
-
