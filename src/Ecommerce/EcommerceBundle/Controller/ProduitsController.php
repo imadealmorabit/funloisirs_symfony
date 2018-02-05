36fc03abd1e94335f3dea99675bcd4086a77e530
imadealmorabit <imadealmorait@gmail.com>
2018-02-05 22:38:12 +0000

    creation d'un menu dynamique

--- src/Ecommerce/EcommerceBundle/Controller/ProduitsController.php
+++ src/Ecommerce/EcommerceBundle/Controller/ProduitsController.php
@@ -6,6 +6,14 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class ProduitsController extends Controller
 {
+	public function categorieAction($categorie)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $produits = $em->getRepository('EcommerceBundle:Produits')->byCategorie($categorie);
+        
+        return $this->render('EcommerceBundle:Default:produits/layout/produits.html.twig', array('produits' => $produits));
+    }
+    
     public function produitsAction()
     {
         return $this->render('EcommerceBundle:Default/produits/layout:produits.html.twig');
