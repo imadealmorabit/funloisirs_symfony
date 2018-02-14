e4e4e3a4c65409ca9f7d0a06d769a46039f7e7fd
imadealmorabit <imadealmorait@gmail.com>
2018-02-14 23:25:43 +0000

    ajout de la fonctionnaliee 'Recherche de produit'

--- src/Ecommerce/EcommerceBundle/Controller/ProduitsController.php
+++ src/Ecommerce/EcommerceBundle/Controller/ProduitsController.php
@@ -3,22 +3,25 @@
 namespace Ecommerce\EcommerceBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Ecommerce\EcommerceBundle\Entity\Produits;
+use Ecommerce\EcommerceBundle\Form\RechercheType;
 
 class ProduitsController extends Controller
 {
-	public function categorieAction($categorie)
+    public function categorieAction($categorie)
     {
         $em = $this->getDoctrine()->getManager();
         $produits = $em->getRepository('EcommerceBundle:Produits')->byCategorie($categorie);
-        
+
         return $this->render('EcommerceBundle:Default:produits/layout/produits.html.twig', array('produits' => $produits));
     }
-    
+
     public function produitsAction()
     {
         $em = $this->getDoctrine()->getManager();
         $produits = $em->getRepository('EcommerceBundle:Produits')->findAll();
-        
+
         return $this->render('EcommerceBundle:Default:produits/layout/produits.html.twig', array('produits' => $produits));
     }
 
@@ -26,7 +29,31 @@ class ProduitsController extends Controller
     {
         $em = $this->getDoctrine()->getManager();
         $produit = $em->getRepository('EcommerceBundle:Produits')->find($id);
-        
+
         return $this->render('EcommerceBundle:Default:produits/layout/presentation.html.twig', array('produit' => $produit));
+    }
+
+    public function rechercheAction()
+    {
+        $form = $this->createForm(RechercheType::class, new Produits());
+
+        return $this->render('EcommerceBundle:Default/recherche/modulesUsed:recherche.html.twig',
+         array('form' => $form->createView()));
+    }
+
+    public function rechercheTraitementAction(Request $request)
+    {
+        $form = $this->createForm(RechercheType::class, new Produits());
+        if ($request->isMethod('POST') && $form->handleRequest($request)->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $produits = $em->getRepository('EcommerceBundle:Produits')->recherche($form['nom']->getData());
+            //var_dump($produits);
+            //die();
+        } else {
+            throw $this->createNotFoundException("la page n'existe pas. ");
+        }
+
+        return $this->render('EcommerceBundle:Default/produits/layout:presentation_recherche.html.twig',
+          array('produits' => $produits));
     }
 }
---
+++ src/Ecommerce/EcommerceBundle/Form/RechercheType.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Ecommerce\EcommerceBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class RechercheType extends AbstractType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->add('nom');
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'Ecommerce\EcommerceBundle\Entity\Produits',
+        ));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getBlockPrefix()
+    {
+        return 'ecommerce_ecommercebundle_produits';
+    }
+}
