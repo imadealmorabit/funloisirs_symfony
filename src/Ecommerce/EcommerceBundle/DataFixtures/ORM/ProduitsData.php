56ae2f99f8d3dde0886469c7d62dd743ecdc4a2b
imadealmorabit <imadealmorait@gmail.com>
2018-02-24 18:59:07 +0000

    ajout de la TVA

--- src/Ecommerce/EcommerceBundle/DataFixtures/ORM/ProduitsData.php
+++ src/Ecommerce/EcommerceBundle/DataFixtures/ORM/ProduitsData.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace Ecommerce\EcommerceBundle\DataFixtures\ORM;
 
 use Doctrine\Common\DataFixtures\AbstractFixture;
@@ -17,12 +18,12 @@ class ProduitsData extends AbstractFixture implements OrderedFixtureInterface
         $produit1->setImage($this->getReference('media3'));
         $produit1->setNom('Poivron rouge');
         $produit1->setPrix('1.99');
-        $produit1->setTva($this->getReference('tva2'));
+        $produit1->setTva($this->getReference('tva1'));
         $manager->persist($produit1);
 
         $produit2 = new Produits();
         $produit2->setCategorie($this->getReference('categorie1'));
-        $produit2->setDescription("Piment est généralement associé à la saveur du piquant (pimenté).");
+        $produit2->setDescription('Piment est généralement associé à la saveur du piquant (pimenté).');
         $produit2->setDisponible('1');
         $produit2->setImage($this->getReference('media4'));
         $produit2->setNom('Piment');
@@ -39,7 +40,7 @@ class ProduitsData extends AbstractFixture implements OrderedFixtureInterface
         $produit3->setPrix('0.99');
         $produit3->setTva($this->getReference('tva2'));
         $manager->persist($produit3);
-        
+
         $produit4 = new Produits();
         $produit4->setCategorie($this->getReference('categorie1'));
         $produit4->setDescription("Le poivron vert est un groupe de cultivars de l'espèce Capsicum annuum.");
@@ -49,17 +50,17 @@ class ProduitsData extends AbstractFixture implements OrderedFixtureInterface
         $produit4->setPrix('2.99');
         $produit4->setTva($this->getReference('tva2'));
         $manager->persist($produit4);
-        
+
         $produit5 = new Produits();
         $produit5->setCategorie($this->getReference('categorie2'));
-        $produit5->setDescription("Le raisin est le fruit de la Vigne. Le raisin de la vigne cultivée Vitis vinifera est un des fruits les plus cultivés au monde, avec 68 millions de tonnes produites en 2010.");
+        $produit5->setDescription('Le raisin est le fruit de la Vigne. Le raisin de la vigne cultivée Vitis vinifera est un des fruits les plus cultivés au monde, avec 68 millions de tonnes produites en 2010.');
         $produit5->setDisponible('1');
         $produit5->setImage($this->getReference('media7'));
         $produit5->setNom('Raisin');
         $produit5->setPrix('0.97');
         $produit5->setTva($this->getReference('tva2'));
         $manager->persist($produit5);
-        
+
         $produit6 = new Produits();
         $produit6->setCategorie($this->getReference('categorie2'));
         $produit6->setDescription("L’orange est un agrume, fruit des orangers, des arbres de différentes espèces de la famille des Rutacées ou d'hybrides de ceux-ci.");
@@ -69,10 +70,10 @@ class ProduitsData extends AbstractFixture implements OrderedFixtureInterface
         $produit6->setPrix('1.20');
         $produit6->setTva($this->getReference('tva2'));
         $manager->persist($produit6);
-        
+
         $manager->flush();
     }
-    
+
     public function getOrder()
     {
         return 4;
