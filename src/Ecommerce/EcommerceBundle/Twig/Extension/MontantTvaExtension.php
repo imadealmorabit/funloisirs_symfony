6f6136878d4cde638868d007c9e3031a5901998f
imadealmorabit <imadealmorait@gmail.com>
2018-02-24 18:40:27 +0000

    ajout de Total HT et Total TTC

---
+++ src/Ecommerce/EcommerceBundle/Twig/Extension/MontantTvaExtension.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Ecommerce\EcommerceBundle\Twig\Extension;
+
+class MontantTvaExtension extends \Twig_Extension
+{
+    public function getFilters()
+    {
+        return array(
+      new \Twig_SimpleFilter('montantTva', array($this, 'montantTva')),
+    );
+    }
+
+    public function montantTva($prixHT, $tva)
+    {
+        return round(($prixHT / $tva) - $prixHT, 2);
+    }
+}
