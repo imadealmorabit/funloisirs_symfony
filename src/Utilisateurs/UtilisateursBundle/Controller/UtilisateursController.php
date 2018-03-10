a88f13c2aae8ab222b4686097678e08e9314c481
imadealmorabit <imadealmorait@gmail.com>
2018-03-10 20:33:34 +0000

    edition des factures en PDF

--- src/Utilisateurs/UtilisateursBundle/Controller/UtilisateursController.php
+++ src/Utilisateurs/UtilisateursBundle/Controller/UtilisateursController.php
@@ -3,6 +3,7 @@
 namespace Utilisateurs\UtilisateursBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
 
 class UtilisateursController extends Controller
 {
@@ -12,5 +13,40 @@ class UtilisateursController extends Controller
         $factures = $em->getRepository('EcommerceBundle:Commandes')->byFacture($this->getUser());
 
         return $this->render('UtilisateursBundle:Default:layout/facture.html.twig', array('factures' => $factures));
+    }
+
+    public function facturesPDFAction($id)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $facture = $em->getRepository('EcommerceBundle:Commandes')->findOneBy(array('utilisateur' => $this->getUser(),
+                                                                                     'valider' => 1,
+                                                                                     'id' => $id, ));
+
+        if (!$facture) {
+            $this->get('session')->getFlashBag()->add('error', 'Une erreur est survenue');
+
+            return $this->redirect($this->generateUrl('facutres'));
+        }
+
+        $html = $this->renderView('UtilisateursBundle:Default:layout/facturePDF.html.twig', array('facture' => $facture));
+
+        /* $html2pdf = new \Html2Pdf_Html2Pdf('P', 'A4', 'fr');
+        $html2pdf->pdf->SetAuthor('Funloisirs4you');
+        $html2pdf->pdf->SetTitle('Facture '.$facture->getReference());
+        $html2pdf->pdf->SetSubject('Facture Funloisirs4you');
+        $html2pdf->pdf->SetKeywords('facture,Funloisirs4you');
+        $html2pdf->pdf->SetDisplayMode('real'); */
+
+        $html2pdf = $this->get('html2pdf_factory')->create('P', 'A4', 'fr', true, 'UTF-8', array(10, 15, 10, 15));
+        
+        $html2pdf->writeHTML($html);
+        $html2pdf->Output('Facture.pdf'); 
+
+        
+
+        $response = new Response();
+        $response->headers->set('Content-type', 'application/pdf');
+
+        return $response;
     }
 }
